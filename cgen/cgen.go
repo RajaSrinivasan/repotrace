094eecde591740e17104cd3299c416e66ec7875b
Srini <s@srin.me>
2019-11-21 16:30:35 -0500

    c generator to support repo

--- cgen/cgen.go
+++ cgen/cgen.go
@@ -38,4 +38,39 @@ func (cg CGen) Generate(v versions.Version, filename string) {
 
 func (cg CGen) GenerateFromRepo(m *repo.Manifest, v versions.Version, filename string) {
 
+	outfilename := filename + ".h"
+	outfile, err := os.Create(outfilename)
+	if err != nil {
+		log.Printf("%v creating %s\n", err, outfilename)
+		return
+	}
+	defer outfile.Close()
+
+	fmt.Fprintln(outfile, "// C header generator")
+	fmt.Fprintf(outfile, "// File: %s\n", outfilename)
+
+	fmt.Fprintf(outfile, "#define BUILD_TIME \"%s\"\n", time.Now().Format("Mon Jan 2 2006 15:04:05"))
+	fmt.Fprintf(outfile, "#define VERSION_MAJOR (%d)\n", v.Major)
+	fmt.Fprintf(outfile, "#define VERSION_MINOR (%d)\n", v.Minor)
+	fmt.Fprintf(outfile, "#define VERSION_BUILD (%d)\n", v.Build)
+
+	for _, prj := range m.Projects {
+		//repo.Show(prj)
+
+		if len(prj.Revision) == 0 {
+			fmt.Fprintf(outfile, "\n// Project %s\n", prj.Name)
+			if len(prj.Repo) > 0 {
+				//prjName := path.Base(prj.Name)
+				fmt.Fprintf(outfile, "#define %sRepoURL \"%s\" \n", prj.Path, prj.Repo)
+				if len(prj.Revision) > 0 {
+					fmt.Fprintf(outfile, "#define %sRevision \"%s\"\n", prj.Path, prj.Revision)
+				}
+
+				fmt.Fprintf(outfile, "#define %sShortCommitId \"%s\" \n", prj.Path, prj.ShortCommitId)
+				fmt.Fprintf(outfile, "#define %sLongCommitId \"%s\" \n", prj.Path, prj.LongCommitId)
+			}
+		} else {
+			fmt.Fprintf(outfile, "\n// Project %s Revision %s \n", prj.Name, prj.Revision)
+		}
+	}
 }
