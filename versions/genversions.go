32a3bbfe1f3f4f8c419ea8e9c1e437e149d23398
Srini <s@srin.me>
2019-11-21 16:59:04 -0500

    fixup version info

--- versions/genversions.go
+++ versions/genversions.go
@@ -1,11 +1,11 @@
 package versions
 // Go package generator
 // File: versions.h
-const buildTime = "Wed Nov 20 2019 18:46:58"
+const buildTime = "Thu Nov 21 2019 16:58:13"
 const versionMajor = 0
 const versionMinor = 0
 const versionBuild = 999
-const repoURL = "git@gitlab.com:privatetutor/projectlets/go.git"
-const branchName = "reposupport"
-const shortCommitId = "971bf5e"
-const longCommitId = "971bf5e417a31e34e22fc16e93efd50dcd2ef9ce"
+const repoURL = "git@github.com:RajaSrinivasan/repotrace.git"
+const branchName = "master"
+const shortCommitId = "119d78a"
+const longCommitId = "119d78a2b146e4f93c31f391794a7e8f4f63613a"
