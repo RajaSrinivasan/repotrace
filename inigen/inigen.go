b83559fa5e287a112dab614da1c33bc0cafd6716
Srini <s@srin.me>
2019-11-21 16:35:25 -0500

    added ini file ability

--- inigen/inigen.go
+++ inigen/inigen.go
@@ -16,7 +16,7 @@ type IniGen int
 //     generates a C header file with the given revision information
 func (ig IniGen) Generate(v versions.Version, filename string) {
 
-	inifile, err := os.Create(filename)
+	inifile, err := os.Create(filename + ".ini")
 	if err != nil {
 		log.Printf("%v creating %s\n", err, filename)
 		return
@@ -36,4 +36,34 @@ func (ig IniGen) Generate(v versions.Version, filename string) {
 
 func (ig IniGen) GenerateFromRepo(m *repo.Manifest, v versions.Version, filename string) {
 
+	outfilename := filename + ".ini"
+	outfile, err := os.Create(outfilename)
+	if err != nil {
+		log.Printf("%v creating %s\n", err, outfilename)
+		return
+	}
+	defer outfile.Close()
+
+	fmt.Fprintf(outfile, "[versions]\n")
+	fmt.Fprintf(outfile, "buildTime = \"%s\"\n", time.Now().Format("Mon Jan 2 2006 15:04:05"))
+	fmt.Fprintf(outfile, "versionMajor = %d\n", v.Major)
+	fmt.Fprintf(outfile, "versionMinor = %d\n", v.Minor)
+	fmt.Fprintf(outfile, "versionBuild = %d\n", v.Build)
+
+	for _, prj := range m.Projects {
+		//repo.Show(prj)
+
+		if len(prj.Revision) == 0 {
+			if len(prj.Repo) > 0 {
+				//prjName := path.Base(prj.Name)
+				fmt.Fprintf(outfile, "%sRepoURL= \"%s\" \n", prj.Path, prj.Repo)
+				if len(prj.Revision) > 0 {
+					fmt.Fprintf(outfile, "%sRevision= \"%s\"\n", prj.Path, prj.Revision)
+				}
+
+				fmt.Fprintf(outfile, "%sShortCommitId= \"%s\" \n", prj.Path, prj.ShortCommitId)
+				fmt.Fprintf(outfile, "%sLongCommitId= \"%s\" \n", prj.Path, prj.LongCommitId)
+			}
+		}
+	}
 }
